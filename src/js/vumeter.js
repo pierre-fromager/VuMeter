c8cc72ef4207334ba9b1fb6e1627390880b618f8
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2020-08-09 00:19:17 +0200

    VuMeter / refac : rename css classname

--- src/js/vumeter.js
+++ src/js/vumeter.js
@@ -1,23 +1,28 @@
+const tooltipAttr = 'data-tooltip';
+const vumHorizClassname = '.vumh';
+const vumHorizOvlClassname = '.vumoverlayh';
+const vumVertClassname = '.vumv';
+const vumVertOvlClassname = '.vumoverlayv';
 const selVms = (sel) => document.querySelectorAll(sel);
 const selOv = (id, sel) => document.getElementById(id).querySelector(sel);
 const setVmValue = (id, sel, vpc) => {
     const ov = selOv(id, sel);
     const vpcf = (100 - vpc) + '%';
-    if (ov.hasAttribute('data-tooltip')) {
-        ov.setAttribute('data-tooltip', vpc + '%');
+    if (ov.hasAttribute(tooltipAttr)) {
+        ov.setAttribute(tooltipAttr, vpc + '%');
     }
-    if (sel == '.hvmov') {
+    if (sel == vumHorizOvlClassname) {
         ov.style.width = vpcf;
     } else {
         ov.style.height = vpcf;
     }
 }
 const rpct = divisor => Math.floor(((Math.random() * 100) + 1) / divisor);
-const updateAllVms = () => {
-    selVms('.hvm').forEach(element => {
-        setVmValue(element.id, '.hvmov', rpct(1))
+const updateAllvums = () => {
+    selVms(vumHorizClassname).forEach(element => {
+        setVmValue(element.id, vumHorizOvlClassname, rpct(1))
     });
-    selVms('.vvm').forEach(element => {
-        setVmValue(element.id, '.vvmov', rpct(1))
+    selVms(vumVertClassname).forEach(element => {
+        setVmValue(element.id, vumVertOvlClassname, rpct(1))
     });
 }
